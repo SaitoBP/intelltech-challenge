6368a131da0cf1b65edae0f9b45bc9c066b4ee98
Gabriel <gabrielpereira050201@hotmail.com>
2021-08-28 10:59:59 -0300

    Added query client and provider

--- client/src/pages/Routes.jsx
+++ client/src/pages/Routes.jsx
@@ -2,43 +2,50 @@ import React from 'react'
 
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import { Directories } from './Directories'
 import { Homepage } from './Homepage'
 import { Shapes } from './Shapes'
 
+/* Create a Client */
+const queryClient = new QueryClient()
+
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
 
-          <li>
-            <Link to='/diretorio'>Diretorios</Link>
-          </li>
+            <li>
+              <Link to='/diretorio'>Diretorios</Link>
+            </li>
 
-          <li>
-            <Link to='/formas'>Formas Geometricas</Link>
-          </li>
-        </ul>
-      </nav>
+            <li>
+              <Link to='/formas'>Formas Geometricas</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Switch>
-        <Route exact path='/'>
-          <Homepage />
-        </Route>
+        <Switch>
+          <Route exact path='/'>
+            <Homepage />
+          </Route>
 
-        <Route path='/diretorio'>
-          <Directories />
-        </Route>
+          <Route path='/diretorio'>
+            <Directories />
+          </Route>
 
-        <Route path='/formas'>
-          <Shapes />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+          <Route path='/formas'>
+            <Shapes />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
