d0771af0280259291cabedaa6c423a6f91a65170
Gabriel <gabrielpereira050201@hotmail.com>
2021-08-28 12:06:42 -0300

    Created basic shapes

--- client/src/pages/Shapes/Shapes.jsx
+++ client/src/pages/Shapes/Shapes.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+
+const Color = ({ color }) => {
+  return (
+    <div
+      style={{
+        width: '15px',
+        height: '15px',
+        backgroundColor: color,
+        borderRadius: '7.5px',
+      }}
+    ></div>
+  )
+}
 
 const Shapes = () => {
+  const [form, setForm] = useState({
+    directoryId: '',
+    name: '',
+    type: '',
+    color: '#000000',
+    size: '',
+  })
+
+  const queryClient = useQueryClient()
+
+  const { status, data } = useQuery('shapes', async () => {
+    const response = await fetch('http://localhost:5000/api/shapes', { method: 'GET' })
+
+    return response.json()
+  })
+
+  const { mutate } = useMutation(
+    async data => {
+      const response = await fetch('http://localhost:5000/api/shapes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+
+      if (!response.ok) {
+        console.log(response)
+        throw new Error('Error when creating new shape')
+      }
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('shapes')
+      },
+    }
+  )
+
+  const submitForm = _e => {
+    _e.preventDefault()
+
+    console.log(form)
+    mutate(form)
+  }
+
+  const handleForm = ({ target: { value, name } }) => {
+    setForm({
+      ...form,
+      [name]: value,
+    })
+  }
+
+  if (status === 'loading') {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div>
       <h1>Formas Geometricas</h1>
+
+      <form onSubmit={submitForm}>
+        <label htmlFor='shapes-directory'>Diretorio</label>
+        <input type='text' name='directoryId' value={form.directoryId} onChange={handleForm} />
+
+        <label htmlFor='shapes-name'>Nome</label>
+        <input type='text' name='name' value={form.name} onChange={handleForm} />
+
+        <label htmlFor='shapes-type'>Tipo</label>
+        <input type='text' name='type' value={form.type} onChange={handleForm} />
+
+        <label htmlFor='shapes-color'>Cor</label>
+        <input type='color' name='color' value={form.color} onChange={handleForm} />
+
+        <label htmlFor='shapes-size'>Tamanho(px)</label>
+        <input type='text' name='size' value={form.size} onChange={handleForm} />
+
+        <button type='submit'>Criar</button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <td>Diretorio</td>
+            <td>Nome</td>
+            <td>Cor</td>
+            <td>Editar</td>
+            <td>Remover</td>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map(row => (
+            <tr key={row.id}>
+              <td>{row.directoryId}</td>
+              <td>{row.name}</td>
+              <td style={{ display: 'flex', alignItems: 'center' }}>
+                {row.color} <Color color={row.color} />
+              </td>
+              <td>[Editar]</td>
+              <td>[Remover]</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -6,23 +6,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const FAKE_DIRECTORIES = [
-  { id: 1, name: 'Diretorio Fake 1' },
-  { id: 2, name: 'Diretorio Fake 2' },
-  { id: 3, name: 'Diretorio Fake 3' },
-  { id: 4, name: 'Diretorio Fake 4' },
-  { id: 5, name: 'Diretorio Fake 5' },
-  { id: 6, name: 'Diretorio Fake 6' },
-  { id: 7, name: 'Diretorio Fake 7' },
-  { id: 8, name: 'Diretorio Fake 8' },
-  { id: 9, name: 'Diretorio Fake 9' },
+const FAKE_DIRECTORIES = [{ id: 1, name: 'Diretorio Fake 1' }]
+
+const FAKE_SHAPES = [
+  {
+    id: 1,
+    directoryId: 1,
+    name: 'Quadrado 1',
+    type: '1-Quadrado',
+    color: '#3dfff5',
+    size: '300',
+  },
 ]
 
 app.get('/api/directories', (req, res) => res.json(FAKE_DIRECTORIES))
 
 app.post('/api/directories', (req, res) => {
-  console.log(req.body)
-
   let newId = FAKE_DIRECTORIES[FAKE_DIRECTORIES.length - 1]
 
   newId = parseInt(newId.id) + 1
@@ -30,6 +29,18 @@ app.post('/api/directories', (req, res) => {
   FAKE_DIRECTORIES.push({ id: newId, name: req.body.name })
 
   res.json(FAKE_DIRECTORIES)
+})
+
+app.get('/api/shapes', (req, res) => res.json(FAKE_SHAPES))
+
+app.post('/api/shapes', (req, res) => {
+  let newId = FAKE_SHAPES[FAKE_SHAPES.length - 1]
+
+  newId = parseInt(newId.id) + 1
+
+  FAKE_SHAPES.push({ id: newId, ...req.body })
+
+  res.json(FAKE_SHAPES)
 })
 
 const PORT = process.env.PORT || 5000
