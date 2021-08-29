e20cd740bba7528603aeb722e6d2b6bfbeffa73a
Gabriel <gabrielpereira050201@hotmail.com>
2021-08-29 17:43:04 -0300

    Added Box and Title components

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import { Box } from '../../components/Box'
+import { Title } from '../../components/Title'
+
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <Box>
+      <Title>Homepage</Title>
+    </Box>
   )
 }
 
