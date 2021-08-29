8af38c610f624adc21007157a7556d977d27708d
Gabriel <gabrielpereira050201@hotmail.com>
2021-08-28 22:44:24 -0300

    Added sqlite db

---
+++ server/src/config/database.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize')
+
+const path = require('path')
+
+console.log()
+
+/* Connect to database */
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: `${path.join(__dirname, '../', '../', '../', 'database')}/data.sqlite`,
+  // logging: false,
+})
+
+/* Test Connection */
+sequelize
+  .authenticate()
+  .then(console.log('Database connected...'))
+  .catch(error => console.error(error))
+
+module.exports = sequelize
